e74674520059e8ef0c8200b9c474eb8343525051
Rotmulaag <0um4shu@gmail.com>
2023-11-26 01:54:54 +0100

    Fixing Bugs

--- app/src/main/java/com/example/relearning3/MainActivity.kt
+++ app/src/main/java/com/example/relearning3/MainActivity.kt
@@ -20,7 +20,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("AAAAAAAA","HEREEEEEEEEEEE")
         setContent {
             RELearning3Theme {
                 // A surface container using the 'background' color from the theme
